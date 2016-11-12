c3457181c82eb4804a1c243f8442413880d898e5
Luiz Eduardo <luizeduardogodoy@gmail.com>
2016-11-12 12:55:42 +0000

    NOvas entities

---
+++ src/Entity/Campanha.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace FidelizeFood\Entity;
+
+class Campanha extends FFADO{
+    
+    
+    var $_table = "campanha";
+    
+    
+}
---
+++ src/Entity/UsuarioCampanha.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace FidelizeFood\Entity;
+
+class UsuarioCampanha extends FFADO{
+	
+	var $_table = "usuariocampanha";
+}
