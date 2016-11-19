ad63a72230562bf896123e0ab2307b236cd51326
Luiz Eduardo Godoy Bueno <Luiz Eduardo Godoy Bueno>
2016-11-19 15:39:02 -0200

    azure

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
-$arr = array("Title" => "Seja Bem vindo - Hospedado no Windows Azure");
+$arr = array("Title" => "Seja Bem vindo - Hospedado no Windows Azure 2016");
 
 print json_encode($arr);
