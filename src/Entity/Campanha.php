d29a2ce4b10cbf9c8e1eb0671ba2625834040db4
luiz <luizeduardogodoy@gmail.com>
2018-04-16 08:29:50 -0300

    ajustes banco novo; construct classe campanha

--- src/Entity/Campanha.php
+++ src/Entity/Campanha.php
@@ -6,6 +6,13 @@ class Campanha extends FFADO{
     
     
     var $_table = "campanha";
+
+    public function __construct(){
+			
+		$this->nameId = "idcampanha";
+		
+		parent::__construct();
+	}
     
     
 }
